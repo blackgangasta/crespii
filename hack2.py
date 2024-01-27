bea915909b06255f50cfe9032e37af4b4b29962c
jix <146824377+blackgangasta@users.noreply.github.com>
2024-01-27 15:04:39 -0500

    hack2.py

--- hack2.py
+++ hack2.py
@@ -245,5 +245,5 @@ dispatcher.add_handler(account_details_conversation_handler)
 dispatcher.add_handler(channel_conversation_handler)
 
 # Start polling for updates
-updater.start_polling()
+updater.start_polling(port=8080)
 updater.idle()
